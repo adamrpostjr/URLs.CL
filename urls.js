909368e0e32bc6bf4647bb77c7769bb2349fd732
Adam <adamrpostjr@gmail.com>
2020-09-19 00:07:16 -0400

    Set validation and got data ready for API

--- urls.js
+++ urls.js
@@ -14,9 +14,8 @@ urls.use(bodyParser.urlencoded({ extended: true }))
 urls.use(express.static('public/assets/'));
 urls.use(bodyParser.json());
 
-const port = process.env.PORT
 const server = urls.listen(8085, function(){
-    console.log("Server started at http://svr-post.com:"+port);
+    console.log("Server started at http://localhost:8085");
 });
 
 
@@ -33,13 +32,10 @@ const schema = yup.object().shape({
 
 // --------------------------------------------------
 
-
 // Index
 urls.get("/", (req, res) => {
    res.sendFile(__dirname+'/public/index.html');
 });
-
-
 urls.get('/favicon.ico', (req,res)=>{
 	return 'your favicon'
 })
@@ -48,10 +44,8 @@ urls.get('/favicon.ico', (req,res)=>{
 urls.get('/:slug', (req,res,next) =>{
 	var slug = req.params.slug
 	console.log('Request: /'+slug);
-
 	Database.find({ slug: slug }, function (err, docs) {
 		if (err) throw err;
-
 		if (docs.length == 1) {
 			console.log('Sent: '+docs[0].ref);
 			res.redirect(docs[0].ref)
@@ -64,20 +58,32 @@ urls.get('/:slug', (req,res,next) =>{
 // New URL
 urls.post('/', slowDown({windowMs: 30 * 1000, delayAfter: 1, delayMs: 500,}), rateLimit({windowMs: 30 * 1000,max: 1}), async (req, res) => {
 	var postReq = {'slug': nanoid(4), 'ref': req.body.url,'uses': 0, 'otu':req.body.otu}
-	
 	const slug = postReq.slug
 	const ref = postReq.ref
 	const otu = postReq.otu
-
-	Database.find({ slug: slug }, function (err, docs) {
-		if (err) throw err;
-		
-		if (docs.length == 1) {
-			console.log('Exists: '+docs[0].ref);
-			res.json('Exists');
-		} else {
-			Database.insert({slug: slug, ref:ref, otu:otu});
-			res.json({'Created': slug, 'For': ref, 'Allowances': otu, 'FullURL': "urls.cl/"+slug})
-		}
-	});
+  try {
+    await schema.validate({
+      slug,
+      ref,
+      otu,
+    })
+    if (ref.includes('urls.cl')) {
+      throw ({'errors':'REALLYYYY? REALLY THO..'});
+    }
+    Database.find({ slug: slug }, function (err, docs) {
+      if (err) throw err;
+      if (docs.length == 1) {
+        console.log('Exists: '+docs[0].ref);
+        res.json('Exists');
+      } else {
+        var date = new Date();
+        var created = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+        Database.insert({slug: slug, ref: ref, otu: otu, uses: 0, created: created});
+        res.json({'Created': slug, 'For': ref, 'Allowances': otu, 'FullURL': "urls.cl/"+slug})
+      }
+    });
+  } catch (e) {
+    console.log(e.errors);
+    // res.json(e.errors)
+  }
 });
