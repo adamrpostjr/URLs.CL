9c1b2da666cda7f15a99838db2b75bf77109add7
Adam <adamrpostjr@gmail.com>
2021-06-17 21:31:12 +0000

    working on getting the process of getting a shortned url worked out

---
+++ src/Shortened.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Alert from "./Alert.svelte";
+
+  export let em;
+  var text
+  var status
+  var alert
+
+  var copy = (es)=>{
+    if (event.button == 2){
+        event.preventDefault();
+        alert = true
+        text = 'copied'
+        status = 1
+        setTimeout(() => {
+          alert = false
+        }, 4000);
+  }
+  }
+</script>
+
+{#if alert}
+  <Alert text={text} status={status}/>
+{/if}
+
+
+<popup >
+  <a href="https://urls.cl/{em}" on:contextmenu|preventDefault={copy}>urls.cl/{em}</a>
+</popup>
+
+<style>
+  popup{
+    z-index: 10;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: xx-large;
+    font-weight: bold;
+    width: 50vw;
+    height: 50vh;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
